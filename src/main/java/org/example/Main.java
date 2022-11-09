f369415961dd8737faa1579cc384c1142c44f93a
daya <dayajuangmahaputra@gmail.com>
2022-11-09 10:18:30 +0900

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,29 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double test1 = area(5);
+        double test2 = area(10,2);
+
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+
+    public static double area(double radius){
+        // Check if the input is valid
+        if(radius < 0)
+            return -1;
+
+        // Calculate the area
+        return radius * radius * Math.PI;
+    }
+
+    public static double area(double x, double y){
+        // Check if the input is valid
+        if(x < 0 || y < 0)
+            return -1;
+
+        // Calculate the area
+        return x * y;
     }
 }
